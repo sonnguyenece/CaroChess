612eb70462af95e65d9ca0d26ee31b4c891cd029
sonnguyenece <sonnguyenece@gmail.com>
2020-03-04 09:07:51 +0700

    caros chess last updated

--- functions.js
+++ functions.js
@@ -26,12 +26,14 @@ function Create2DArray(rows) {
     let arr = [];
     for (let i = 0; i < rows; i++) {
         arr[i] = [];
+        // if(i===rows-1)arr[i+1]="";
     }
+    arr.push("");
     return arr;
 }
 
 function drawXO(id) {
-    if(checkWin())return;
+    if (checkWin()) return;
     let temp = document.getElementById(id).id;
     temp = temp.split(".");
     i = temp[0];
@@ -44,7 +46,6 @@ function drawXO(id) {
         document.getElementById(id).innerText = 'X';
         document.getElementById(id).style.color = "blue";
         table[i][j] = 'X';
-        console.log(table)
     } else {
         document.getElementById(id).innerText = 'O';
         document.getElementById(id).style.color = "red";
@@ -56,7 +57,8 @@ function drawXO(id) {
 }
 
 function checkWin() {
-    if (checkRow() || checkCol() || checkRightObl() || checkLeftObl()) {
+    if (checkRow() || checkCol() || checkRightObl() || checkLeftObl())
+    {
         setTimeout(function () {
             alert("Game Over");
         }, 100);
@@ -69,16 +71,20 @@ function checkRow() {
     let k = 1;
     for (let i = 0; i < row; i++) {
         for (let j = 0; j < col - 4; j++) {
-            for (let k = 1; k <= 4; k++) {
-                if (table[i][j] === table[i][j + k]) {
-                    count++;
-                }
+            while (table[i][j] === table[i][j + k]) {
+                k++;
+                count++;
             }
-            if (count === 4) {
-
+            if (count >= 4) {
+                for (k = 0; k <= count; k++) {
+                    document.getElementById(i + '.' + (j + k)).style.color = "yellow";
+                }
                 return true;
             }
-            if (count < 4) count = 0;
+            if (count < 4) {
+                count = 0;
+                k = 1;
+            }
         }
     }
 }
@@ -88,16 +94,22 @@ function checkCol() {
     let count = 0;
     let k = 1;
     for (let j = 0; j < col; j++) {
-        for (let i = 0; i < row - 4; i++) {
-            for (let k = 1; k <= 4; k++) {
-                if (table[i][j] === table[i + k][j]) {
-                    count++;
-                }
+        for (let i = 0; i < row-4; i++) {
+            while (table[i][j] === table[i+k][j]) {
+                //khi o ria border +1 se lam gia tri cua table[i] vuot ra ngoai dan den bug
+                k++;
+                count++;
             }
-            if (count === 4) {
+            if (count >= 4) {
+                for (k = 0; k <= count; k++) {
+                    document.getElementById((i+k) + '.' +j).style.color = "yellow";
+                }
                 return true;
             }
-            if (count < 4) count = 0;
+            if (count < 4) {
+                count = 0;
+                k = 1;
+            }
         }
     }
 }
@@ -107,15 +119,20 @@ function checkRightObl() {
     let k = 1;
     for (let i = 0; i < row - 4; i++) {
         for (let j = 0; j < col - 4; j++) {
-            for (let k = 1; k <= 4; k++) {
-                if (table[i][j] === table[i + k][j + k]) {
-                    count++;
-                }
+            while (table[i][j] === table[i+k][j+k]) {
+                k++;
+                count++;
             }
-            if (count === 4) {
+            if (count >= 4) {
+                for (k = 0; k <= count; k++) {
+                    document.getElementById((i+k) + '.' +(j+k)).style.color = "yellow";
+                }
                 return true;
             }
-            if (count < 4) count = 0;
+            if (count < 4) {
+                count = 0;
+                k = 1;
+            }
         }
     }
 }
@@ -125,15 +142,20 @@ function checkLeftObl() {
     let k = 1;
     for (let i = 0; i < row - 4; i++) {
         for (let j = 4; j < col; j++) {
-            for (let k = 1; k <= 4; k++) {
-                if (table[i][j] === table[i + k][j - k]) {
-                    count++;
-                }
+            while (table[i][j] === table[i+k][j-k]) {
+                k++;
+                count++;
             }
-            if (count === 4) {
+            if (count >= 4) {
+                for (k = 0; k <= count; k++) {
+                    document.getElementById((i+k) + '.' +(j-k)).style.color = "yellow";
+                }
                 return true;
             }
-            if (count < 4) count = 0;
+            if (count < 4) {
+                count = 0;
+                k = 1;
+            }
         }
     }
 }
@@ -142,8 +164,6 @@ function restart() {
     display = "<table border='1px' cellpadding='2px' " +
         "style='text-align: center;border-collapse: collapse;table-layout: fixed'><tr>";
     tempOX = 0;
-    col = prompt("Nhap vao so cot : ");
-    row = prompt("Nhap vao so hang : ");
     table = Create2DArray(row);
     init();
 }
